b0d37d9d5beac75c50049119ba65d37f91abc19f
valmobil <valmobil@gmail.com>
2018-07-15 16:23:12 +0300

    Adoptation for Heroku v2 Maven Update First Page teaser with Maven Corrected

--- src/main/java/Servlets/ServletMessanger.java
+++ src/main/java/Servlets/ServletMessanger.java
@@ -4,7 +4,6 @@ import DAO.ChatDAO;
 import DAO.MessagesDAO;
 import Models.Messages;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
